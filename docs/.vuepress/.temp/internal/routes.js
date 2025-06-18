e537139212e565583dd70a7de9bc1261edfedacf
xuyp <xuyp@iynss.com>
2025-06-18 17:07:12 +0800

    feat:更新路由

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,24 +1,71 @@
-export const redirects = JSON.parse("{}")
+export const redirects = JSON.parse("{}");
 
 export const routes = Object.fromEntries([
-  ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"D:/project/open-source/code/github/m-admin-doc/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/project/open-source/code/github/m-admin-doc/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
-  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/project/open-source/code/github/m-admin-doc/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+  [
+    "/get-started.html",
+    {
+      loader: () =>
+        import(
+          /* webpackChunkName: "get-started.html" */ "D:/project/open-source/code/docs/docs/.vuepress/.temp/pages/get-started.html.js"
+        ),
+      meta: { title: "快速开始" },
+    },
+  ],
+  [
+    "/",
+    {
+      loader: () =>
+        import(
+          /* webpackChunkName: "index.html" */ "D:/project/open-source/code/docs/docs/.vuepress/.temp/pages/index.html.js"
+        ),
+      meta: { title: "主页" },
+    },
+  ],
+  [
+    "/guide.html",
+    {
+      loader: () =>
+        import(
+          /* webpackChunkName: "index.html" */ "D:/project/open-source/code/docs/docs/.vuepress/.temp/pages/guide.html.js"
+        ),
+      meta: { title: "指南" },
+    },
+  ],
+  [
+    "/m-table.html",
+    {
+      loader: () =>
+        import(
+          /* webpackChunkName: "index.html" */ "D:/project/open-source/code/docs/docs/.vuepress/.temp/pages/m-table.html.js"
+        ),
+      meta: { title: "MTable" },
+    },
+  ],
+  [
+    "/404.html",
+    {
+      loader: () =>
+        import(
+          /* webpackChunkName: "404.html" */ "D:/project/open-source/code/docs/docs/.vuepress/.temp/pages/404.html.js"
+        ),
+      meta: { title: "" },
+    },
+  ],
 ]);
 
 if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
+  import.meta.webpackHot.accept();
   if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRoutes(routes);
   }
   if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects);
   }
 }
 
 if (import.meta.hot) {
   import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
+    __VUE_HMR_RUNTIME__.updateRoutes(routes);
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects);
+  });
 }
