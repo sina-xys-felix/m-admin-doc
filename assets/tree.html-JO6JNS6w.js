9ebf13b263a2cf9b4fdfd29944d7f42905c89cd8
sina-xys-felix <106717071+sina-xys-felix@users.noreply.github.com>
2025-06-20 03:54:21 +0000

    Deploying to gh-pages from @ sina-xys-felix/m-admin-doc@8d52c7e54f2c779d9847b408283db6e35f98632b 🚀

---
+++ assets/tree.html-JO6JNS6w.js
@@ -0,0 +1,39 @@
+import{_ as s,c as a,a as e,o as i}from"./app-DGtc3a_m.js";const l={};function p(t,n){return i(),a("div",null,n[0]||(n[0]=[e(`<h1 id="目录结构" tabindex="-1"><a class="header-anchor" href="#目录结构"><span>目录结构</span></a></h1><p><strong>M-Admin 目录结构说明</strong></p><div class="language-javascript line-numbers-mode" data-highlighter="prismjs" data-ext="js"><pre><code class="language-javascript"><span class="line"><span class="token constant">M</span><span class="token operator">-</span>Admin</span>
+<span class="line">├─ config                       ## Vite配置</span>
+<span class="line">├─ <span class="token keyword">public</span>                       ## 静态资源文件 </span>
+<span class="line">├─ src                          </span>
+<span class="line">│  ├─ api                       ## <span class="token constant">API</span>接口管理</span>
+<span class="line">│  ├─ assets                    ## 静态资源文件</span>
+<span class="line">│  ├─ common                    ## 公共常量</span>
+<span class="line">│  ├─ components                ## 项目组件</span>
+<span class="line">│  ├─ config                    ## 项目全局配置</span>
+<span class="line">│  ├─ directive                 ## 自定义指令</span>
+<span class="line">│  ├─ hooks                     ## 常用 hooks</span>
+<span class="line">│  ├─ layout                    ## 项目布局文件</span>
+<span class="line">│  ├─ locale                    ## 国际化配置</span>
+<span class="line">│  ├─ mock                      ## mock 接口</span>
+<span class="line">│  ├─ router                    ## router 路由管理</span>
+<span class="line">│  ├─ store                     ## Pinia Store</span>
+<span class="line">│  ├─ types                     ## 项目全局types</span>
+<span class="line">│  ├─ utils                     ## 常用工具类</span>
+<span class="line">│  ├─ views                     ## 页面</span>
+<span class="line">│  ├─ App<span class="token punctuation">.</span>vue                   ## 项目主文件</span>
+<span class="line">│  ├─ env<span class="token punctuation">.</span>d<span class="token punctuation">.</span>ts                  ## 指定ts 识别</span>
+<span class="line">│  └─ main<span class="token punctuation">.</span>ts                   ## 项目入口文件</span>
+<span class="line">├─ <span class="token punctuation">.</span>env<span class="token punctuation">.</span>development             ## 开发环境配置</span>
+<span class="line">├─ <span class="token punctuation">.</span>env<span class="token punctuation">.</span>production              ## 生产环境配置</span>
+<span class="line">├─ <span class="token punctuation">.</span>eslintignore                ## 忽略 EsLint 配置 </span>
+<span class="line">├─ <span class="token punctuation">.</span>eslintrc<span class="token punctuation">.</span>js                 ## Eslint 校验配置文件 </span>
+<span class="line">├─ <span class="token punctuation">.</span>gitignore                   ## 忽略 Git 配置</span>
+<span class="line">├─ <span class="token punctuation">.</span>prettierignore              ## 忽略 Prettier 格式化</span>
+<span class="line">├─ <span class="token punctuation">.</span>prettierrc<span class="token punctuation">.</span>js               ## Prettier 格式化配置</span>
+<span class="line">├─ <span class="token punctuation">.</span>stylelintrc<span class="token punctuation">.</span>js              ## Stylelint 样式格式化配置</span>
+<span class="line">├─ auto<span class="token operator">-</span>imports<span class="token punctuation">.</span>d<span class="token punctuation">.</span>ts            ## auto<span class="token operator">-</span>imports 自动导入配置</span>
+<span class="line">├─ babel<span class="token punctuation">.</span>config<span class="token punctuation">.</span>js              ## babel 配置</span>
+<span class="line">├─ commitlint<span class="token punctuation">.</span>config<span class="token punctuation">.</span>js         ## Git 提交规范</span>
+<span class="line">├─ index<span class="token punctuation">.</span>html                   ## 入口html</span>
+<span class="line">├─ <span class="token keyword">package</span><span class="token punctuation">.</span>json                 ## 项目依赖包管理</span>
+<span class="line">├─ <span class="token constant">README</span><span class="token punctuation">.</span>md                    ## <span class="token constant">README</span> 介绍</span>
+<span class="line">└─ tsconfig<span class="token punctuation">.</span>json                ## TypeScript 配置</span>
+<span class="line"></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3)]))}const o=s(l,[["render",p]]),u=JSON.parse('{"path":"/md/tree.html","title":"目录结构","lang":"zh-CN","frontmatter":{},"git":{"updatedTime":1750391629000,"contributors":[{"name":"xuyp","username":"xuyp","email":"xuyp@iynss.com","commits":1,"url":"https://github.com/xuyp"}],"changelog":[{"hash":"8d52c7e54f2c779d9847b408283db6e35f98632b","time":1750391629000,"email":"xuyp@iynss.com","author":"xuyp","message":"feat: 更新文档"}]},"filePathRelative":"md/tree.md"}');export{o as comp,u as data};
